3d7f8bec561ff554eb0af5df5aca06ff6d05963e
Rar4ik <Rar4ik@github.com>
2020-02-19 22:56:09 +0300

    Добавьте файлы проекта.

---
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace WebStore.Clients.Base
+{
+    public abstract class BaseClient : IDisposable
+    {
+        protected readonly HttpClient _Client;
+        protected readonly string _ServiceAdress;
+
+        protected BaseClient(IConfiguration config, string ServiceAdress)
+        {
+            _ServiceAdress = ServiceAdress;
+            _Client = new HttpClient()
+            {
+                BaseAddress = new Uri(config["WebApiUrl"])
+            };
+            var header = _Client.DefaultRequestHeaders.Accept;
+            header.Clear();
+            header.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        #region Disposing
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private bool _Disposed;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_Disposed || !disposing) return;
+            _Disposed = true;
+            _Client.Dispose();
+        }
+        #endregion
+    }
+}
---
+++ UI/WebStore/Controllers/WebApiTestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.Api;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebStore.Controllers
+{
+    public class WebApiTestController : Controller
+    {
+        private readonly IValueService _ValueService;
+
+        public WebApiTestController(IValueService valueService)
+        {
+            _ValueService = valueService;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var value = _ValueService.Get();
+            return View(value);
+        }
+    }
+}
